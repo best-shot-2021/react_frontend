1edbb097e26bb4003ce579e6203b46e62f51168a
sanakang0615 <82388712+sanakang0615@users.noreply.github.com>
2022-01-17 04:44:39 +0900

    Finalized the homepage

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,6 +1,6 @@
 // Home.js
 import React from 'react';
-import img from './assets/images/home_img1.png';
+import img from './assets/images/cells2.png';
 import { Link } from 'react-router-dom';
 import ImageButton from 'react-image-button';
 import imgButton from './assets/images/logo.svg';
@@ -14,25 +14,14 @@ const Home = () => {
     <div className="Home1">
       <header className="Home1-header">
 
-        {/* <img src={img} className="Background" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+         <img src={img} className="Background" alt="logo" />
         <ul>
 
           <Link to="/chat">
             <button class="chatbtn">
-              <img src= {btnimg}>
+              <img src= {btnimg} width="280">
+                {/* <MdAdd /> */}
               </img>
-              {/* <MdAdd /> */}
             </button>
           </Link>
       </ul>
