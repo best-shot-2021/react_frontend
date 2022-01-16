b40fea2b3e2d342011690b7c4f642aa6ba60ba35
sanakang0615 <82388712+sanakang0615@users.noreply.github.com>
2022-01-17 03:23:37 +0900

    added btn

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,62 +7,22 @@ import './index.css';
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
+import Chat from './Chat';
+import {MdAdd} from 'react-icons/md';
 
 function App() {
-  const [img, setImage] = useState({});
-  const onChange = (e) => {
-    setImage(e.target.files[0]);
-  }
-  const onClick = async () => {
-    const formData = new FormData();
-    formData.append('img_file', img);
-    // 서버의 upload API 호출
-    const res = await axios.post('http://192.249.18.213:80/face_classifier', formData);
-    console.log(res);
-  }
-  const steps = [
-    {
-      id: '0',
-      message: 'Welcome to react chatbot!',
-      trigger: '1',
-    },
-    {
-      id: '1',
-      message: 'Bye!',
-      end: true,
-    },
-  ];
   return (
-    /*<div className="App">
-      <input type='file'
-        accept='image/*'  
-        onChange={onChange}>
-      </input>
-      <button onClick={onClick}>제출</button>
-    </div>*/
     <div className="App">
-      
-
     <BrowserRouter>
-    <ul>
-        <li>
-          <Link to="/">홈</Link>
-        </li>
-        <li>
-          <Link to="/about">어바웃</Link>
-        </li>
-      </ul>
-        <Routes>
-          <Route exact path="/" element={<Home/>}></Route>
-          <Route exact path="/about" element={<About/>}></Route>
-        </Routes>
+      <Routes>
+        <Route exact path="/" element={<Home/>}></Route>
+        <Route exact path="/about" element={<About/>}></Route>
+        <Route exact path="/chat" element={<Chat/>}></Route>
+      </Routes>
+
     </BrowserRouter> 
     </div> 
     
-    // <div>
-    //   <ChatBot steps={steps} />
-    //   <InfoForm/>
-    // </div>
   );
 }
 export default App;
--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,13 +1,20 @@
 // Home.js
 import React from 'react';
 import img from './assets/images/home_img1.png';
+import { Link } from 'react-router-dom';
+import ImageButton from 'react-image-button';
+import imgButton from './assets/images/logo.svg';
+import {MdAdd} from 'react-icons/md';
+import btnimg from './assets/images/btnimg1.png';
+import './App.css';
 //import styled, { ThemeProvider } from "styled-components";
 
 const Home = () => {
   return (
     <div className="Home1">
       <header className="Home1-header">
-        <img src={img} className="Background" alt="logo" />
+
+        {/* <img src={img} className="Background" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,7 +26,18 @@ const Home = () => {
         >
           Learn React
         </a>
+        <ul>
+
+          <Link to="/chat">
+            <button class="chatbtn">
+              <img src= {btnimg}>
+              </img>
+              {/* <MdAdd /> */}
+            </button>
+          </Link>
+      </ul>
       </header>
+      
     </div>
   );
 };
