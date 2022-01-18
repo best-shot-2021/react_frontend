eaf9441a3ed2b584e0f490a20b2f90fdb7d72a4b
sanakang0615 <82388712+sanakang0615@users.noreply.github.com>
2022-01-18 16:04:52 +0900

    home6 final

--- frontend/src/Chat.js
+++ frontend/src/Chat.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import './index.css';
-import ChatForm from './ChatForm';
+import ChatForm from './components/ChatForm';
 
 function Chat() {
   return (
