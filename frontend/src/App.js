9bac1f387a39c91c51e7cc1f36c5c7704c89c435
Hans-0101 <hyhsumin@naver.com>
2022-01-18 18:07:39 +0900

    feat:router error fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 // import React from 'react';
 import './App.css';
+import axios from 'axios';
+import ChatBot from 'react-simple-chatbot';
+import ChatForm from './components/ChatForm';
 import './index.css';
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
-// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './Home';
-import About from './About';
 import Chat from './Chat';
 import Result from './Result';
 import React from "react";
@@ -12,16 +13,12 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<Home/>}></Route>
-          <Route exact path="/about" element={<About/>}></Route>
-          <Route exact path="/chat" element={<Chat/>}></Route>
-          <Route exact path="/result" element={<Result/>}></Route>
-        </Routes>
-      </BrowserRouter> 
-    </div> 
-    
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/chat" element={<Chat/>} />
+        <Route path="/result" element={<Result/>} />
+      </Routes>
+    </div>     
   );
 }
 export default App;
--- frontend/src/Result.js
+++ frontend/src/Result.js
@@ -6,36 +6,38 @@ import ModalMovie from "./components/ModalMovie";
 import {getMovies} from "./api";
 import StarfieldAnimation from 'react-starfield-animation';
 
+
 export default function Result() {
-    const [movies, setMovies] = useState([])
-    
-    const fetchMovies = async () =>{
-      try {
-        const data = await getMovies();
-        setMovies(data.results)
-      }catch(e){
-      }
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = async () =>{
+    try {
+      const data = await getMovies();
+      setMovies(data.results)
+    }catch(e){
     }
+  }
+    
+  useEffect(()=>{
+    fetchMovies();
+  }, [])
+  return (
+    <>
+      <StarfieldAnimation lineWidth = {2.0} numParticles = {130} alphaFactor = {0.35}
+        style={{
+          position: 'absolute',
+          width: '100%',
+          height: '350%',
+        }}
+      />
+      <Navbar />
+      <ModalMovie />
       
-      useEffect(()=>{
-        fetchMovies();
-      }, [])
-      return (
-        <>
-          <StarfieldAnimation lineWidth = {2.0} numParticles = {130} alphaFactor = {0.35}
-            style={{
-              position: 'absolute',
-              width: '100%',
-              height: '350%',
-            }}
-          />
-          <Navbar />
-          <ModalMovie />
-          
-          <div className="Result">
-            <Movies movies={movies}/>
-          </div>
-        </>
-      )
-    }
-    
+
+      <div className="Result">
+        <Movies movies={movies}/>
+      </div>
+    </>
+  )
+}
+
